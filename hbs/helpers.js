e72a2ef3443fa293b272f19a14d0899b60ed4647
KevinJairChuquitarco <kevinchuquitarco11@gmail.com>
2021-08-05 13:08:47 -0500

    Version

--- hbs/helpers.js
+++ hbs/helpers.js
@@ -4,3 +4,9 @@ hbs.registerHelper('getAnio',()=>{
     return new Date().getFullYear();
 });
 
+hbs.registerHelper('getProductos',(options)=>{
+    const fs = require('fs');
+    let rawdata = fs.readFileSync('./data/productos.json');
+    let productos=JSON.parse(rawdata);
+    return options.fn(productos);
+});
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 
 const hbs = require('hbs');
 require('./hbs/helpers')
+
 const port = process.env.PORT || 3000 ; 
 
 app.use(express.static(__dirname + '/public'));
@@ -11,13 +12,8 @@ hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine','hbs');
 
-const fs = require('fs');
-let rawdata = fs.readFileSync(__dirname+'/data/productos.json');
-let productos=JSON.parse(rawdata);
-
-
 app.get('/',(req,res)=>{
-    res.render('index',productos);
+    res.render('index');
 });
 
 app.listen(port,()=>{
